ddfa6f19de32f5d2b557b5734a630f8966c47c12
paularah <p.arah@alustudent.com>
2021-12-28 13:16:03 +0200

    .

--- qstn2.py
+++ qstn2.py
@@ -1,7 +1,7 @@
 def gradeStudent(grades):
     """
     seperaing the algorithm for rounding a student grades into a seperate 
-    function then calls the algorithm on the arrya of grdades returning 
+    function then calls the algorithm on the array of grdades returning 
     a new array of the rounded grades.
     Big O -> O(n)
     """
